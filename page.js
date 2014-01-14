376b8d4621042f9a8be6475c57d037bc34e905a9
Larry Price <larry.price.dev@gmail.com>
2014-01-13 22:03:16 -0500

    making coordinate system

--- page.js
+++ page.js
@@ -1,5 +1,26 @@
-function draw_b() {
-  var b_canvas = document.getElementById("b");
-  var b_context = b_canvas.getContext("2d");
-  b_context.fillRect(50, 25, 150, 100);
+function draw_vertical_lines(c) {
+  for (var x = 0.5; x < 500; x += 10) {
+    c.moveTo(x, 0);
+    c.lineTo(x, 375);
+  }
 }
+
+function draw_horizontal_lines(c) {
+  for (var y = 0.5; y < 375; y += 10) {
+    c.moveTo(0, y);
+    c.lineTo(500, y)
+  }
+}
+
+function draw_paths() {
+  var c_canvas = document.getElementById("c");
+  var context = c_canvas.getContext("2d");
+
+  draw_vertical_lines(context);
+  draw_horizontal_lines(context);
+
+  context.strokeStyle = "#eee";
+  context.stroke();
+}
+
+draw_paths();
