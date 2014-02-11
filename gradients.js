6c460c87a83683d21c4b47a0e023301d66e27911
Larry Price <larry.price.dev@gmail.com>
2014-02-11 10:14:01 -0500

    gradients vertical and diaganol

--- gradients.js
+++ gradients.js
@@ -1,9 +1,23 @@
 
-function create_gradient(context) {
+function create_horizontal_gradient(context) {
   var my_gradient = context.createLinearGradient(0, 0, 300, 0);
-  my_gradient.addColorStop(0, "black");
-  my_gradient.addColorStop(1, "white");
-  context.fillStyle = my_gradient;
+  create_gradient(my_gradient, context);
+}
+
+function create_vertical_gradient(context) {
+  var my_gradient = context.createLinearGradient(0, 0, 0, 225);
+  create_gradient(my_gradient, context);
+}
+
+function create_diaganol_gradient(context) {
+  var my_gradient = context.createLinearGradient(0, 0, 300, 255);
+  create_gradient(my_gradient, context);
+}
+
+function create_gradient(gradient, context) {
+  gradient.addColorStop(0, "black");
+  gradient.addColorStop(1, "white");
+  context.fillStyle = gradient;
   context.fillRect(0, 0, 300, 225);
 }
 
@@ -11,7 +25,7 @@ function draw() {
   var d_canvas = document.getElementById("d");
   var context = d_canvas.getContext("2d");
 
-  create_gradient(context);
+  create_diaganol_gradient(context);
 }
 
 draw();
